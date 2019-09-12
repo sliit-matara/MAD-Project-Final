f17e4081268ac0e4db518537f48d38fe64fa5eec
ahkam-9408 <it18098174@my.sliit.lk>
2019-09-12 20:34:34 +0530

    Ahkam 3rd Edit

--- app/src/main/java/com/example/mad_miniproject/MoneyTransSuccess.java
+++ app/src/main/java/com/example/mad_miniproject/MoneyTransSuccess.java
@@ -2,13 +2,30 @@ package com.example.mad_miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MoneyTransSuccess extends AppCompatActivity {
+public class MoneyTransSuccess extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money_trans_success);
+
+        btnDone = findViewById(R.id.btnDone);
+
+        btnDone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btnDone){
+            Intent main = new Intent(this,MainActivity.class);
+            startActivity(main);
+        }
     }
 }

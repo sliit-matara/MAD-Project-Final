b12545529686a993d4e55a1b97aeaed3db5056c5
ahkam-9408 <it18098174@my.sliit.lk>
2019-09-12 19:19:13 +0530

    Ahkam 1st Edit

---
+++ app/src/main/java/com/example/mad_miniproject/MoneyTransOwnAccount.java
@@ -0,0 +1,14 @@
+package com.example.mad_miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoneyTransOwnAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_money_trans_own_account);
+    }
+}

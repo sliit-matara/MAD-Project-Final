bec4a65725913a98ab9942aad91ee91f97c02d5a
ahkam-9408 <it18098174@my.sliit.lk>
2019-09-12 20:18:27 +0530

    Ahkam 2nd Edit

--- app/src/main/java/com/example/mad_miniproject/MoneyTransMain.java
+++ app/src/main/java/com/example/mad_miniproject/MoneyTransMain.java
@@ -2,13 +2,35 @@ package com.example.mad_miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MoneyTransMain extends AppCompatActivity {
+public class MoneyTransMain extends AppCompatActivity implements View.OnClickListener {
+
+    private Button ownAcc,otherAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money_trans_main);
+
+        ownAcc = (Button) findViewById(R.id.btnOwnAcc);
+        otherAcc = (Button) findViewById(R.id.btnOtherAcc);
+
+        ownAcc.setOnClickListener(this);
+        otherAcc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btnOwnAcc){
+            Intent ownAccount = new Intent(this,MoneyTransOwnAccount.class);
+            startActivity(ownAccount);
+        }else if(view.getId()==R.id.btnOtherAcc){
+            Intent otherAccount = new Intent(this,MoneyTransOtherAccount.class);
+            startActivity(otherAccount);
+        }
     }
 }
--- app/src/main/java/com/example/mad_miniproject/MoneyTransOwnAccount.java
+++ app/src/main/java/com/example/mad_miniproject/MoneyTransOwnAccount.java
@@ -2,13 +2,30 @@ package com.example.mad_miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MoneyTransOwnAccount extends AppCompatActivity {
+public class MoneyTransOwnAccount extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnTransfer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money_trans_own_account);
+
+        btnTransfer = (Button) findViewById(R.id.btnTransfer);
+
+        btnTransfer.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btnTransfer){
+            Intent success = new Intent(this,MoneyTransSuccess.class);
+            startActivity(success);
+        }
     }
 }
